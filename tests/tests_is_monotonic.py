ef457c38bb3948aabe66068491ee3faa4d91a9a5
Art-Invis <151573705+Art-Invis@users.noreply.github.com>
2024-04-30 21:48:41 +0300

    Update tests_is_monotonic.py

--- tests/tests_is_monotonic.py
+++ tests/tests_is_monotonic.py
@@ -1,6 +1,12 @@
 import unittest
-from is_monotonic_array import is_monotonic
+import sys
+import os
 
+script_dir = os.path.dirname(os.path.relpath(__file__))
+sys.path.append(os.path.dirname(script_dir))
+
+
+from src.is_monotonic_array import is_monotonic
 
 class TestIsMonotonic(unittest.TestCase):
 
