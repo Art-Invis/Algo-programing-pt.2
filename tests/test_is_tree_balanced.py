600b5c8e6dfa66626ab9b42f7f730f7784c8dac2
Art-Invis <151573705+Art-Invis@users.noreply.github.com>
2024-05-02 21:08:02 +0300

    Update test_is_tree_balanced.py

--- tests/test_is_tree_balanced.py
+++ tests/test_is_tree_balanced.py
@@ -1,5 +1,10 @@
 import unittest
-from balanced_tree import is_tree_balanced, BinaryTree
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.relpath(__file__))
+sys.path.append(os.path.dirname(script_dir))
+from src.is_tree_balanced import is_tree_balanced, BinaryTree
 
 
 class TestIsTreeBalanced(unittest.TestCase):
@@ -28,6 +33,20 @@ class TestIsTreeBalanced(unittest.TestCase):
         
     def test_single_node_tree(self):
         root = BinaryTree(1)
+        self.assertTrue(is_tree_balanced(root))
+
+    def test_given_tree(self):
+        root = BinaryTree(40)
+        root.left = BinaryTree(20)
+        root.right = BinaryTree(50)
+        root.left.left = BinaryTree(5)
+        root.left.right = BinaryTree(30)
+        root.right.left = BinaryTree(45)
+        root.right.right = BinaryTree(60)
+        root.left.right.left = BinaryTree(25)
+        root.left.right.right = BinaryTree(32)
+        root.right.right.right = BinaryTree(55)
+
         self.assertTrue(is_tree_balanced(root))
 
 if __name__ == "__main__":
