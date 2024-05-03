5e71c003ed41cd725c567f1a1978430a7c951cd9
Art-Invis <151573705+Art-Invis@users.noreply.github.com>
2024-05-03 08:28:07 +0300

    Update test_red_black_priority_queue.py

--- tests/test_red_black_priority_queue.py
+++ tests/test_red_black_priority_queue.py
@@ -20,16 +20,6 @@ class TestRedBlackTree(unittest.TestCase):
         self.assertEqual(tree.delete_max(), (5, 9))
         self.assertEqual(tree.view_queue(), [(3, 8), (7, 6), (9, 5), (2, 4), (8, 3)])
 
-    def test_insert_and_delete(self):
-        tree = RedBlackTree()
-        tree.insert(5, 10)
-        tree.insert(3, 8)
-        tree.insert(7, 6)
-        self.assertEqual(tree.view_queue(), [(5, 10), (3, 8), (7, 6)])
-        self.assertEqual(tree.delete_max(), (7, 6))
-        self.assertEqual(tree.delete_min(), (3, 8))
-        self.assertEqual(tree.view_queue(), [(5, 10)])
-
 
 if __name__ == "__main__":
     unittest.main()
