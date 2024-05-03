62e424cf0a7bf6a314290b69bd8d4c64c5fb2c18
Art-Invis <151573705+Art-Invis@users.noreply.github.com>
2024-05-03 08:26:04 +0300

    Update test_red_black_tree.py

--- tests/test_red_black_tree.py
+++ tests/test_red_black_tree.py
@@ -1,6 +1,10 @@
 import unittest
-from red_black_priority_queue import RedBlackTree
+import os
+import sys
 
+script_dir = os.path.dirname(os.path.relpath(__file__))
+sys.path.append(os.path.dirname(script_dir))
+from src.red_black_priority_queue import RedBlackTree
 
 class TestRedBlackTree(unittest.TestCase):
 
@@ -15,6 +19,16 @@ class TestRedBlackTree(unittest.TestCase):
         tree.insert(2, 4)
         self.assertEqual(tree.delete_max(), (5, 9))
         self.assertEqual(tree.view_queue(), [(3, 8), (7, 6), (9, 5), (2, 4), (8, 3)])
+
+    def test_insert_and_delete(self):
+        tree = RedBlackTree()
+        tree.insert(5, 10)
+        tree.insert(3, 8)
+        tree.insert(7, 6)
+        self.assertEqual(tree.view_queue(), [(5, 10), (3, 8), (7, 6)])
+        self.assertEqual(tree.delete_max(), (7, 6))
+        self.assertEqual(tree.delete_min(), (3, 8))
+        self.assertEqual(tree.view_queue(), [(5, 10)])
 
 
 if __name__ == "__main__":
