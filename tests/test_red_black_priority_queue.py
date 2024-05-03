41fbcf4909deb3743bc3033357f8ef23a8c4546d
Art-Invis <151573705+Art-Invis@users.noreply.github.com>
2024-05-03 09:11:09 +0300

    Add extra test

--- tests/test_red_black_priority_queue.py
+++ tests/test_red_black_priority_queue.py
@@ -7,7 +7,22 @@ sys.path.append(os.path.dirname(script_dir))
 from src.red_black_priority_queue import RedBlackTree
 
 class TestRedBlackTree(unittest.TestCase):
-
+    def test_insert_in_tree(self):
+        tree = RedBlackTree()
+        tree.insert(10, 2)
+        tree.insert(4, 9)
+        tree.insert(5, 8)
+        tree.insert(1, 6)
+        self.assertEqual(tree.view_queue(), [(4, 9), (5, 8), (1, 6), (10, 2)])
+    
+    def test_delete_from_tree(self):
+        tree = RedBlackTree()
+        tree.insert(10, 2)
+        tree.insert(4, 9)
+        tree.insert(5, 8)
+        tree.insert(1, 6)
+        self.assertEqual(tree.delete_max(), (4, 9))
+        
     def test_peek_sequence(self):
         tree = RedBlackTree()
         tree.insert(9, 5)
@@ -19,7 +34,6 @@ class TestRedBlackTree(unittest.TestCase):
         tree.insert(2, 4)
         self.assertEqual(tree.delete_max(), (5, 9))
         self.assertEqual(tree.view_queue(), [(3, 8), (7, 6), (9, 5), (2, 4), (8, 3)])
-
-
+    
 if __name__ == "__main__":
     unittest.main()
