d7a1ba04496d9ab9758b7f86cda9f56141b28d61
Art-Invis <151573705+Art-Invis@users.noreply.github.com>
2024-05-12 22:53:49 +0300

    Add extra changes

--- tests/test_kmp_algorithm.py
+++ tests/test_kmp_algorithm.py
@@ -1,6 +1,10 @@
 import unittest
-from kmp_algorithm import compute_pref_array, search_by_kmp
+import os
+import sys
 
+script_dir = os.path.dirname(os.path.relpath(__file__))
+sys.path.append(os.path.dirname(script_dir))
+from src.kmp_algorithm import compute_pref_array, search_by_kmp
 
 class TestKMP(unittest.TestCase):
     def test_compute_pref_array(self):
@@ -10,11 +14,13 @@ class TestKMP(unittest.TestCase):
 
     def test_search_by_kmp(self):
         self.assertEqual(search_by_kmp("banana", ""), [])
-        self.assertEqual(search_by_kmp("", "apple"), None)
-        self.assertEqual(search_by_kmp("banana", "orange"), None)
         self.assertEqual(search_by_kmp("banana", "na"), [2, 4])
         self.assertEqual(search_by_kmp("mississippi", "issi"), [1, 4])
+        self.assertEqual(search_by_kmp("hello world", "world"), [6])    
 
+    def test_empty_search_by_kmp(self):
+        self.assertEqual(search_by_kmp("", "apple"), None)
+        self.assertEqual(search_by_kmp("banana", "orange"), None)
 
 if __name__ == "__main__":
     unittest.main()
