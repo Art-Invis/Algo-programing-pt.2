6457cb40485bb0ea1d64f7ed9472d3a853094e54
Art-Invis <151573705+Art-Invis@users.noreply.github.com>
2024-05-16 20:35:33 +0300

    Update test_min_length_of_cable_between_wells.py

--- tests/test_min_length_of_cable_between_wells.py
+++ tests/test_min_length_of_cable_between_wells.py
@@ -10,17 +10,17 @@ from src.min_length_of_cable_between_wells import calculate_min_cable_length_fro
 
 class MinCableLength(unittest.TestCase):
     def test_random_wells(self):
-        csv_path = os.path.join(script_dir, '..', 'resources', 'communication_wells.csv')
+        csv_path = "resources/communication_wells.csv"
         result = calculate_min_cable_length_from_csv(csv_path)
         self.assertEqual(result, 3000)
 
     def test_unconnected_wells(self):
-        csv_path = os.path.join(script_dir, '..', 'resources', 'communication_unconnected_wells.csv')
+        csv_path = "resources/communication_unconnected_wells.csv"
         result = calculate_min_cable_length_from_csv(csv_path)
         self.assertEqual(result, -1)
 
     def test_empty_graph(self):
-        csv_path = os.path.join(script_dir, '..', 'resources', 'communication_empty_graphs.csv')
+        csv_path = "resources/communication_empty_graphs.csv"
         result = calculate_min_cable_length_from_csv(csv_path)
         self.assertEqual(result, -1)
 
