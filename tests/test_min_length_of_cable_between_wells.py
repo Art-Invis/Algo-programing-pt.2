9253afc5183f94f2df2fb7aa5a1491ff9611a943
Art-Invis <151573705+Art-Invis@users.noreply.github.com>
2024-05-16 18:35:17 +0300

    Add import changes

--- tests/test_min_length_of_cable_between_wells.py
+++ tests/test_min_length_of_cable_between_wells.py
@@ -1,32 +1,29 @@
-
 import unittest
 import os
 import sys
 
-script_dir = os.path.dirname(os.path.relpath(__file__))
-sys.path.append(os.path.dirname(script_dir))
-from src.min_length_of_cable_between_wells import calculate_min_cable_length_from_csv
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(script_dir)
+sys.path.append(parent_dir)
 
+from src.min_length_of_cable_between_wells import calculate_min_cable_length_from_csv
 
 class MinCableLength(unittest.TestCase):
     def test_random_wells(self):
-        result = calculate_min_cable_length_from_csv(
-            "D:\\Projects\\Algo-programing-pt.2\\resources\\communication_wells.csv"
-        )
+        csv_path = os.path.join(script_dir, '..', 'resources', 'communication_wells.csv')
+        result = calculate_min_cable_length_from_csv(csv_path)
         self.assertEqual(result, 3000)
 
     def test_unconnected_wells(self):
-        result = calculate_min_cable_length_from_csv(
-            "D:\\Projects\\Algo-programing-pt.2\\resources\\communication_unconnected_wells.csv"
-        )
+        csv_path = os.path.join(script_dir, '..', 'resources', 'communication_unconnected_wells.csv')
+        result = calculate_min_cable_length_from_csv(csv_path)
         self.assertEqual(result, -1)
 
     def test_empty_graph(self):
-        result = calculate_min_cable_length_from_csv(
-            "D:\\Projects\\Algo-programing-pt.2\\resources\\communication_empty_graphs.csv"
-        )
+        csv_path = os.path.join(script_dir, '..', 'resources', 'communication_empty_graphs.csv')
+        result = calculate_min_cable_length_from_csv(csv_path)
         self.assertEqual(result, -1)
-
 
 if __name__ == "__main__":
     unittest.main()
+
